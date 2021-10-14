f3c64cf1c1e6505014a07c4f806645e6294d9f6a
arthur-art <arthurteixeira.guts@gmail.com>
2021-10-14 17:44:48 -0300

    Retornando user filtrado pelo email

--- src/index.js
+++ src/index.js
@@ -1,15 +1,64 @@
 const {ApolloServer, gql} = require('apollo-server');
 
+const users = [
+    {
+        _id: String(Math.random()),
+        name: "Arthur",
+        email: "arthur.guts@gmail.com",
+        age: 25,
+        active: true
+    },
+    {
+        _id: String(Math.random()),
+        name: "Joohny",
+        email: "joohny.guts@gmail.com",
+        age: 25,
+        active: true
+    },
+    {
+        _id: String(Math.random()),
+        name: "Ivar",
+        email: "ivar.guts@gmail.com",
+        age: 25,
+        active: true
+    }
+]
+
 
 const typeDefs = gql`
+
+    type User {
+        _id: ID!
+        name: String!
+        email: String!
+        age: Int!
+        active: Boolean!
+    }
+
+    type Post {
+        _id: ID!
+        title: String
+        content: String
+        author: User
+    }
+
     type Query{
-        hello:String
+        hello:String 
+        users: [User!]!
+        getUserByEmail(email:String!): User!
     }
 `;
 
+
 const resolvers = {
     Query:{
-        hello: () => "Hello world"
+        hello: () => "Hello world",
+        users: () => users,
+        getUserByEmail: (_, args) =>{
+            return users.find((user)=>{
+               return user.email === args.email
+            });
+        }
     }
 };
 
