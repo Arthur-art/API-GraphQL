b868cc21466e942051f8836274e40d525dedc58a
arthur-art <arthurteixeira.guts@gmail.com>
2021-10-18 14:26:17 -0300

    Trabalhando com mongodb

---
+++ GraphQLApplyNodejs-Rocketseat/src/resolvers.js
@@ -0,0 +1,35 @@
+const User = require("./User");
+
+const users = [
+    {
+        _id: 0.2727316853963053,
+        name: "Arthur",
+        email: "arthur.guts@gmail.com",
+        age: 25,
+        active: true
+    },
+    {
+        _id: String(Math.random()),
+        name: "Joohny",
+        email: "joohny.guts@gmail.com",
+        age: 25,
+        active: true
+    },
+    {
+        _id: String(Math.random()),
+        name: "Ivar",
+        email: "ivar.guts@gmail.com",
+        age: 25,
+        active: true
+    }
+]
+module.exports = {
+    Query:{
+        users: () => User.find(),
+        user: (_,args)=> User.findById(args.id)
+    },
+
+    Mutation:{
+        createUser: (_,{name, age, email}) => User.create({name, age, email})
+    }
+}
--- GraphQLApplyNodejs-Rocketseat/src/server.js
+++ GraphQLApplyNodejs-Rocketseat/src/server.js
@@ -1,7 +1,18 @@
-import {GraphQLServer} from "graphql-yoga"
+const {GraphQLServer} = require("graphql-yoga");
+const path =  require("path");
+const resolvers = require("./resolvers");
+const mongoose = require("mongoose");
+const User = require("./User");
+
+mongoose.connect("mongodb://localhost:27017/graphqlrocketseat", {
+    maxPoolSize: 50,
+    wtimeoutMS: 2500,
+    useNewUrlParser: true
+});
 
 const server = new GraphQLServer({
-
+ typeDefs: path.resolve(__dirname, "schema.graphql"),
+ resolvers
 });
 
 server.start();
