33a873170cee4f42c5fc16453df6b061e6551a5f
arthur-art <arthurteixeira.guts@gmail.com>
2021-10-19 16:44:10 -0300

    Passando variables como parametro da mutation

--- 4-React-GraphQL/reactgraphql/src/components/mutations/CreateUser.js
+++ 4-React-GraphQL/reactgraphql/src/components/mutations/CreateUser.js
@@ -5,17 +5,23 @@ import {
   } from "@apollo/client";  
 
 const CREATE_USER = gql`
-  mutation{
-  createUser(name:"devReact2",age:25,email:"devreact@gmail.com"){
+  mutation CreateUser($name: String!,$age: Int!,$email: String!){
+    createUser(name:$name,age: $age,email: $email){
     name
     email
     _id
   }
-}
+  }
 `;
 
 export const User = ()=>{
-    const [mutateFunction, { data, loading, error }] = useMutation(CREATE_USER);
+    const [mutateFunction, { data, loading, error }] = useMutation(CREATE_USER, {
+            variables:{
+                name:"Arthur Teixeira",
+                age: 25,
+                email: "arthurteixeira@gmail.com"
+            }
+    });
 
     return (
         <>
