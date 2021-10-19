aa638825bf08c4dc325f7e5a834d9b1530fd1022
arthur-art <arthurteixeira.guts@gmail.com>
2021-10-19 11:01:19 -0300

    Tratando erros com useQuery do clientApollo

--- 4-React-GraphQL/reactgraphql/src/App.js
+++ 4-React-GraphQL/reactgraphql/src/App.js
@@ -6,19 +6,23 @@ import {
 export function App() {
   const { loading, error, data } = useQuery(DATA_GRAPHQL);
 
+  if(data){
+    console.log(data?.users)
+  }
+
   const users = data?.users;
   
-  if(users){
-    users.map((value)=>{
-      console.log(value.name);
-    })
+  if(error){
+    console.log("Error => ",error)
   }
 
   return (
     <>
+    {loading && <p>Carregando...</p>}
     {users && users.map((value)=>{
-      return <li>{value.name}</li>
+      return <li key={value._id}> Name: {value.name} / Email: {value.email}</li>
     })}
+    {error && <p>Ops, Ocorreu um erro ao tentar fazer um request na sua api :(</p>}
     </>
   );
 }
@@ -27,6 +31,8 @@ const DATA_GRAPHQL =  gql`
       query{
   users{
     name
+    _id
+    emai
   }
 }
     `;
