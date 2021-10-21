f97b4c496f25ab03533479ba5f4237add6e7ecf8
arthur-art <arthurteixeira.guts@gmail.com>
2021-10-21 10:04:38 -0300

    Refatorando mutation CreateUser

--- 4-React-GraphQL/reactgraphql/src/components/mutations/CreateUser.js
+++ 4-React-GraphQL/reactgraphql/src/components/mutations/CreateUser.js
@@ -3,6 +3,7 @@ import {
     gql,
     useMutation
   } from "@apollo/client";  
+import React,{useState} from "react"
 
 const CREATE_USER = gql`
   mutation CreateUser($name: String!,$age: Int!,$email: String!){
@@ -15,16 +16,23 @@ const CREATE_USER = gql`
 `;
 
 export const User = ()=>{
+    const [name, setName] = useState("")
+    const [age, setAge] = useState(0)
+    const [email, setEmail] = useState("")
+
     const [mutateFunction, { data, loading, error }] = useMutation(CREATE_USER, {
             variables:{
-                name:"Arthur Teixeira",
-                age: 25,
-                email: "arthurteixeira@gmail.com"
+                name:name,
+                age: Number(age),
+                email: email
             }
     });
 
     return (
         <>
+        <input value={name} type="text" placeholder="name" onChange={({target}) => setName(target.value)} />
+        <input value={age} type="number" placeholder="age" onChange={({target}) => setAge(target.value)} />
+        <input value={email} type="text" placeholder="email" onChange={({target}) => setEmail(target.value)} />
         <button onClick={mutateFunction}>Criar um novo user</button>
         </>
     )
